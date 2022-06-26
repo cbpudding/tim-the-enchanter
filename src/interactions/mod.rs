a2382f0f4260fe017752b9d0bb2ebc9349bb1892
NicksWorld <nickmcdaniel00@gmail.com>
2022-06-26 15:39:03 -0400

    Fix responding to interactions & restructure program

---
+++ src/interactions/mod.rs
@@ -0,0 +1,35 @@
+use std::{error::Error, sync::Arc};
+
+use async_trait::async_trait;
+use twilight_http::Client as HttpClient;
+use twilight_interactions::command::{ApplicationCommandData, CreateCommand};
+use twilight_model::application::interaction::{ApplicationCommand, Interaction};
+
+mod role;
+pub use role::Command as RoleCommand;
+
+pub fn get_commands() -> [ApplicationCommandData; 1] {
+    [RoleCommand::create_command()]
+}
+
+mod interaction_prelude {
+    pub use async_trait::async_trait;
+    pub use twilight_http::{Client as HttpClient};
+    pub use twilight_interactions::command::{CommandModel, CreateCommand};
+    pub use twilight_model::{
+        application::interaction::{ApplicationCommand, Interaction},
+        http::interaction::{InteractionResponse, InteractionResponseType},
+    };
+    pub use twilight_util::builder::InteractionResponseDataBuilder;
+
+    pub (super) use crate::{TIM, TW};
+}
+
+#[async_trait]
+pub trait TimInteraction {
+    async fn exec(
+        interaction: Interaction,
+        command: Box<ApplicationCommand>,
+        http: Arc<HttpClient>,
+    ) -> Result<(), Box<dyn Error + Send + Sync>>;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use futures::stream::StreamExt;
+#![allow(clippy::unreadable_literal)]
+
+use futures::{stream::StreamExt, TryFutureExt};
+use interactions::TimInteraction;
 use std::{error::Error, fs, sync::Arc};
 use twilight_cache_inmemory::{InMemoryCache, ResourceType};
 use twilight_gateway::{
     cluster::{Cluster, ShardScheme},
     Event, Intents,
 };
-use twilight_http::{request::AuditLogReason, Client as HttpClient};
-use twilight_interactions::command::{CommandModel, CreateCommand};
+use twilight_http::{Client as HttpClient};
 use twilight_model::{
-    application::interaction::Interaction,
+    application::{interaction::Interaction},
     gateway::payload::incoming::InteractionCreate,
-    guild::Role,
     id::{
         marker::{ApplicationMarker, GuildMarker},
         Id,
@@ -20,70 +21,40 @@ use twilight_model::{
 const TIM: Id<ApplicationMarker> = Id::new(990443765468631081);
 const TW: Id<GuildMarker> = Id::new(565884670424645652);
 
-#[derive(CommandModel, CreateCommand, Debug)]
-#[command(name = "role", desc = "Assign or unassign a role to a user")]
-struct RoleCommand {
-    /// The role to be assigned or unassigned
-    role: Role,
+mod interactions;
+
+#[allow(clippy::single_match)]
+async fn handle_interaction(
+    interaction: Interaction,
+    http: Arc<HttpClient>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Interaction::ApplicationCommand(command) = interaction.clone() {
+        match command.data.name.as_str() {
+            "role" => interactions::RoleCommand::exec(interaction, command, http).await?,
+            _ => (),
+        }
+    }
+    Ok(())
 }
 
+#[allow(clippy::single_match)]
 async fn handle_event(
     _shard_id: u64,
     event: Event,
     http: Arc<HttpClient>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let ia_http = http.interaction(TIM);
     match event {
         Event::InteractionCreate(InteractionCreate(interaction)) => {
-            let token = interaction.token();
-            if let Interaction::ApplicationCommand(command) = interaction.clone() {
-                match command.data.name.as_str() {
-                    "role" => {
-                        let request = RoleCommand::from_interaction(command.data.into())?;
-                        if let Some(member) = command.member {
-                            if let Some(victim) = member.user {
-                                if member.roles.contains(&request.role.id) {
-                                    http.remove_guild_member_role(TW, victim.id, request.role.id)
-                                        .reason("Unassigned by user")?
-                                        .exec()
-                                        .await?;
-                                    ia_http
-                                        .create_followup(token)
-                                        .content(
-                                            format!("\"{}\" unassigned.", request.role.name)
-                                                .as_str(),
-                                        )?
-                                        .exec()
-                                        .await?;
-                                } else {
-                                    http.add_guild_member_role(TW, victim.id, request.role.id)
-                                        .reason("Assigned by user")?
-                                        .exec()
-                                        .await?;
-                                    ia_http
-                                        .create_followup(token)
-                                        .content(
-                                            format!("\"{}\" assigned.", request.role.name).as_str(),
-                                        )?
-                                        .exec()
-                                        .await?;
-                                }
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-            }
+            handle_interaction(interaction, http).await
         }
-        _ => {}
+        _ => Ok(()),
     }
-    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     // Read the token from a text file
-    let token = fs::read_to_string("token.txt")?;
+    let token = fs::read_to_string("token.txt")?.trim().to_string();
     // Tell Discord how we're going to create shards(single share only)
     let scheme = ShardScheme::Range {
         from: 0,
@@ -110,13 +81,15 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         .resource_types(ResourceType::MESSAGE)
         .build();
     // Register commands with Discord
-    let command_role = RoleCommand::create_command();
-    let commands = &[command_role.into()];
-    interaction.set_guild_commands(TW, commands).exec().await?;
+    let commands = interactions::get_commands().map(std::convert::Into::into);
+    interaction.set_guild_commands(TW, &commands).exec().await?;
     // Into the event loop!
     while let Some((shard_id, event)) = events.next().await {
         cache.update(&event);
-        tokio::spawn(handle_event(shard_id, event, Arc::clone(&http)));
+        tokio::spawn(
+            handle_event(shard_id, event, Arc::clone(&http))
+                .map_err(|err| println!("Error processing event: {:?}", err)),
+        );
     }
     Ok(())
 }
